a4ba3063e881342bfc839feb976ab8c5ff788962
Wellerson96 <w.cst96@gmail.com>
2024-09-23 11:51:04 +0000

    Introdução switch

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -11,17 +11,17 @@ int main() {
 
     
     // Cadastro das Cartas:
-    char estado1 = 'A';
-    char codigo1[3] = "A01";
-    char cidade1[30] = "Campinas";
+    char estado1;
+    char codigo1;
+    char cidade1;
     float populacao1;
     float area1;
     float pib1;
     int turismo1;
 
     char estado2;
-    char codigo2[3];
-    char cidade2[30];
+    char codigo2;
+    char cidade2;
     float populacao2;
     float area2;
     float pib2;
@@ -29,7 +29,52 @@ int main() {
 
     int opcao;
 
-    printf("Seja bem vindo ao jogo Super Trunfo Países!!\n");
+    printf("Seja bem vindo ao jogo Super Trunfo Países!!\n\n");
+    printf("Por favor, escolha uma opção:\n");
+    printf("1. Iniciar jogo\n");
+    printf("2. Regras\n");
+    printf("Escolha:\n");
+    scanf("%d", &opcao);
+
+    switch(opcao) {
+        case 1:
+        printf("Iniciando jogo...\n\n");
+        printf("Insira os dados da 1ª carta:\n");
+
+        printf("Digite o estado: \n");
+        scanf("%s", &estado1);
+
+        printf("Digite o código da carta: \n");
+        scanf("%s", &codigo1);
+
+        printf("Digite o nome da cidade: \n");
+        scanf("%s", &cidade1);
+
+        printf("Digite a população: \n");
+        scanf("%f", populacao1);
+
+        printf("Digite a área: \n");
+        scanf("%f", &area1);
+
+        printf("Digite o PIB: \n");
+        scanf("%f", &pib1);
+
+        printf("Digite a quantidade de pontos turísticos: \n");
+        scanf("%d", &turismo1);
+
+        printf("Carta cadastrada com sucesso!!\n\n");
+        printf("Informações da carta:\n");
+
+        printf("Estado: %s", estado1);
+
+
+
+
+
+
+
+    }
+
     
 
     // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
